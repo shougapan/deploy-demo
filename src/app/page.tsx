eadf220a77001e01ecdc5a2fa7bb7ed1751de917
unknown <david.rowlands@live.com>
2025-04-01 11:40:18 +0900

    Change made

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
 <div className="max-w-2xl mx-auto p-6 bg-white rounded-lg shadow-lg">
-  <h2 className="text-3xl font-bold text-gray-800 mb-4">Hello, welcome to the deployment demo.</h2>
+  <h2 className="text-3xl font-bold text-gray-800 mb-4">DIFFERENT.</h2>
   <p className="text-lg text-gray-600 mb-6">The purpose of this page is to deploy a very basic Next.js website.</p>
   
   <p className="text-lg text-gray-600 mb-4">You will learn:</p>
